8278a9222f2a91b79410c4b5e46d58b0dc1174da
Squidfishrl <ivo.genchev4@gmail.com>
2022-07-14 21:28:44 +0300

    add day 3 hw

---
+++ hw/03_day/02_float_comparison.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <float.h>
+
+/* TODO:
+ * convert to integer?
+ * built-in ==
+ * random epsilon
+ * optimal epsilon from float.h (DBL_EPSILON )
+ * dynamic epsilon from float.h (multiply by the larger value)
+ */
+
+int main (int argc, char *argv[])
+{
+    return 0;
+}
+
